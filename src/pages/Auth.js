ba208072d4677167ba4238ddabfdf2e3d276aeb8
dinaelhyate <dinaelhyate@gmail.com>
2024-10-21 10:42:57 +0100

    add auth

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,22 +1,51 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Auth() {
+  const navigate = useNavigate(); // Initialiser useNavigate
   const [emailOrPhoneFocused, setEmailOrPhoneFocused] = useState(false);
   const [usernameFocused, setUsernameFocused] = useState(false);
   const [passwordFocused, setPasswordFocused] = useState(false);
-  const [isLogin, setIsLogin] = useState(true); // État pour basculer entre connexion et inscription
+  const [isLogin, setIsLogin] = useState(true);
 
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    const userData = {
+      username,
+      email,
+      password
+    };
+  
+    console.log('Données envoyées :', userData);
+  
+    try {
+      const response = await axios.post('http://localhost:8085/api/users', userData);
+      console.log('Utilisateur créé avec succès :', response.data);
+
+      // Rediriger vers la page d'accueil après une création réussie
+      navigate('/'); // Remplacez '/home' par le chemin correct de votre page d'accueil
+    } catch (error) {
+      console.error('Erreur lors de la création de l’utilisateur :', error);
+      if (error.response) {
+        console.error('Détails de la réponse d\'erreur :', error.response.data);
+        console.error('Statut de la réponse :', error.response.status);
+      }
+    }
+  };
+  
   return (
     <div className="auth-page">
       <div className="auth-container">
         <div className="login-form">
-
           <Link to="/" className="logo">F<span>oo</span>diesHub</Link>
-
           <h2>{isLogin ? 'Se connecter' : 'Inscription'}</h2>
-          
-          <form>
+          <form onSubmit={handleSubmit}>
             {isLogin ? (
               <>
                 <div className="input-group">
@@ -26,12 +55,13 @@ export default function Auth() {
                     placeholder=" "
                     onFocus={() => setEmailOrPhoneFocused(true)}
                     onBlur={() => setEmailOrPhoneFocused(false)}
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
                   />
                   <label htmlFor="email-or-phone" className={emailOrPhoneFocused ? 'focused' : ''}>
                     Email ou téléphone
                   </label>
                 </div>
-
                 <div className="input-group">
                   <input
                     type="text"
@@ -39,12 +69,13 @@ export default function Auth() {
                     placeholder=" "
                     onFocus={() => setUsernameFocused(true)}
                     onBlur={() => setUsernameFocused(false)}
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
                   />
                   <label htmlFor="username" className={usernameFocused ? 'focused' : ''}>
                     Nom d'utilisateur
                   </label>
                 </div>
-
                 <div className="input-group">
                   <input
                     type="password"
@@ -52,12 +83,13 @@ export default function Auth() {
                     placeholder=" "
                     onFocus={() => setPasswordFocused(true)}
                     onBlur={() => setPasswordFocused(false)}
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
                   />
                   <label htmlFor="password" className={passwordFocused ? 'focused' : ''}>
                     Mot de passe
                   </label>
                 </div>
-
                 <div className="forgot-password">
                   <a href="/forgot-password">Mot de passe oublié ?</a>
                 </div>
@@ -71,12 +103,13 @@ export default function Auth() {
                     placeholder=" "
                     onFocus={() => setUsernameFocused(true)}
                     onBlur={() => setUsernameFocused(false)}
+                    value={username} 
+                    onChange={(e) => setUsername(e.target.value)} 
                   />
                   <label htmlFor="new-username" className={usernameFocused ? 'focused' : ''}>
                     Nom d'utilisateur
                   </label>
                 </div>
-                
                 <div className="input-group">
                   <input
                     type="email"
@@ -84,12 +117,13 @@ export default function Auth() {
                     placeholder=" "
                     onFocus={() => setEmailOrPhoneFocused(true)}
                     onBlur={() => setEmailOrPhoneFocused(false)}
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
                   />
                   <label htmlFor="new-email" className={emailOrPhoneFocused ? 'focused' : ''}>
                     Email
                   </label>
                 </div>
-
                 <div className="input-group">
                   <input
                     type="password"
@@ -97,6 +131,8 @@ export default function Auth() {
                     placeholder=" "
                     onFocus={() => setPasswordFocused(true)}
                     onBlur={() => setPasswordFocused(false)}
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
                   />
                   <label htmlFor="new-password" className={passwordFocused ? 'focused' : ''}>
                     Mot de passe
@@ -104,12 +140,10 @@ export default function Auth() {
                 </div>
               </>
             )}
-
             <button type="submit" className="btn">
               {isLogin ? 'Connexion' : 'S’inscrire'}
             </button>
           </form>
-
           <div className="signup">
             <p>
               {isLogin ? (
@@ -124,7 +158,6 @@ export default function Auth() {
             </p>
           </div>
         </div>
-
         <div className="description">
           <h3>Nous sommes plus qu'une simple entreprise</h3>
           <p>
@@ -144,17 +177,16 @@ export default function Auth() {
           margin-bottom: 500px;
           margin-top: 30px;
         }
-         .logo{
-            font-size: 1.5em;
-            font-weight: bolder;
-            align-items: center;
-            color: var(--primary-color);
-            text-shadow: 0 1px 3px var(--shadow-color);
-            span{
-                color: var(--text-color);
-            }
+        .logo {
+          font-size: 1.5em;
+          font-weight: bolder;
+          align-items: center;
+          color: var(--primary-color);
+          text-shadow: 0 1px 3px var(--shadow-color);
         }
-
+        .logo span {
+          color: var(--text-color);
+        }
         .auth-container {
           display: flex;
           max-width: 800px;
@@ -164,30 +196,25 @@ export default function Auth() {
           border-radius: 10px;
           box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
         }
-
         .login-form, .description {
           flex: 1;
           padding: 20px;
         }
-
         .login-form {
           border-right: 1px solid #ccc; 
         }
-
         .login-form h2 {
           margin-top: 80px;
-          margin-bottom:20px;
+          margin-bottom: 20px;
           font-size: 1.2em; 
           font-weight: normal;
           text-align: center;
         }
-
         .input-group {
           position: relative;
           margin-bottom: 20px; 
           text-align: left;
         }
-
         .input-group label {
           position: absolute;
           left: 10px;
@@ -196,72 +223,59 @@ export default function Auth() {
           color: #999;
           transition: 0.2s ease all;
         }
-
         .input-group input {
           width: 100%;
           padding: 10px;
           font-size: 1em;
           border: 1px solid #ccc;
           border-radius: 5px;
-          outline: none;
-          transition: border-color 0.2s ease; 
         }
-
         .input-group input:focus {
-          border-color: black; 
+          border-color: var(--primary-color);
+          outline: none;
         }
-
         .input-group input:focus + label,
         .input-group input:not(:placeholder-shown) + label {
           top: -10px;
+          left: 10px;
           font-size: 0.8em;
-          color: black; 
+          color: var(--primary-color);
         }
-
         .forgot-password {
-          margin-bottom: 20px;
           text-align: right;
+          margin-top: 10px;
         }
-
-        .forgot-password a {
-          color: #007bff;
-          text-decoration: none;
-        }
-
-        .btn {
-          width: 100%;
-          padding: 10px;
-          background-color: var(--primary-color);
-          color: white;
-          border: none;
-          border-radius: 5px;
-          font-size: 1em;
-          cursor: pointer;
-        }
-
         .signup {
+          text-align: center;
           margin-top: 20px;
         }
-
-        .signup a {
-          color: #007bff;
-          text-decoration: none;
-        }
-
         .description {
-          background-color: var(--primary-color);
-          color: white;
           text-align: center;
+          padding-left: 40px;
+          background-color: var(--primary-color);
+          color:white;
         }
-
         .description h3 {
           margin-top: 100px;
         }
-
         .description p {
           font-size: 0.9em;
-          line-height: 1.5;
-          margin-top: 60px;
+           margin-top: 50px;
+             color:white;
+        }
+        .btn {
+          width: 100%;
+          padding: 10px;
+          font-size: 1em;
+          color: white;
+          background-color: var(--primary-color);
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          transition: background-color 0.3s ease;
+        }
+        .btn:hover {
+          background-color: darken(var(--primary-color), 10%);
         }
       `}</style>
     </div>
